385b9698379c3129b92c8dd6b9ba67f76233155f
ariftoteles <arif.santoso4@gmail.com>
2021-05-25 13:24:09 +0700

    change method get network management

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const PORT = 5001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/DepositSystem-api/NetworkManagement", async (req, res) => {
+app.get("/DepositSystem-api/NetworkManagement", async (req, res) => {
 	try {
 		const result = await axios({
 			method: "POST",
 			url: `${process.env.BASE_URL}/DepositSystem-api/NetworkManagement?`,
 		});
-		console.log(result);
-		if (result) res.status(200).json(result);
+		console.log({ data });
+		if (data) res.status(200).json(data);
 	} catch (error) {
 		console.log(error);
 		res.status(400).json(error);
